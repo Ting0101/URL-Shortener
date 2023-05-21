19a81082cee168750809e0838deceb6dddce9754
Ting0101 <microdaisy@gmail.com>
2023-05-21 14:44:58 +0800

    feat: use short URL(outputURL) link to original URL(input)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const shortenURL = require('./models/shortenURL')
 const generateRandomString = require('./generateRandomString')
 const bodyParser = require('body-parser')
 const port = 3000
+const server = `http://localhost:3000/`
 
 const app = express()
 
@@ -32,13 +33,21 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   // console.log(req.body)
-  const inputURL = req.body.inputURL 
+  const inputURL = req.body.inputURL
   let randomString = generateRandomString()
-  const outputURL = `https//localhost:3000/`+`${randomString}`
+  const outputURL = server + `${randomString}`
 
   shortenURL.create({ inputURL, outputURL })
-    .then(() => res.render('show', { successURL:outputURL }))
+    .then(() => res.render('show', { successURL: outputURL }))
     .catch(error => { console.log('error') })
+})
+
+app.get('/:randomString', (req, res) => {
+  const string = req.params.randomString
+  const outputURL = server + string
+  shortenURL.findOne({ outputURL })
+    .then((shortenURL) => res.redirect(shortenURL.inputURL))
+    .catch((err) => console.log(err))
 })
 
 app.listen(port, () => {
