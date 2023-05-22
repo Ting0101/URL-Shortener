85d7cb349bc7b1fa692b4a469cf7d7e729c505f6
Ting0101 <microdaisy@gmail.com>
2023-05-22 17:15:01 +0800

    refactor: express routes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const shortenURL = require('./models/shortenURL')
-const generateRandomString = require('./generateRandomString')
 const bodyParser = require('body-parser')
 const port = 3000
-const server = `http://localhost:3000/`
+const routes = require('./routes')
 
 require('./config/mongoose')
 const app = express()
@@ -12,41 +10,8 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 
-
-app.get('/', (req, res) => {
-  res.render('index', {})
-})
-
-//輸入網址後資料庫先查詢inputURL是否存在，
-//若存在輸出outputURL，若不存在則創建一個存於資料庫中
-app.post('/', (req, res) => {
-  const inputURL = req.body.inputURL
-  let randomString = generateRandomString()
-  const outputURL = server + `${randomString}`
-
-  shortenURL.findOne({ inputURL })
-    .lean()
-    .then((item) => {
-      if (item) {
-        res.render('show', { successURL: item.outputURL })
-      } else {
-        shortenURL.create({ inputURL, outputURL })
-          .then(() => res.render('show', { successURL: outputURL }))
-          .catch(error => { console.log('create error') })
-      }
-    })
-    .catch(error => { console.log('error') })
-})
-
-//短網址輸入後，資料庫查找後直接輸出，連結原始網址
-app.get('/:string', (req, res) => {
-  const string = req.params.string
-  const outputURL = server + string
-  shortenURL.findOne({ outputURL })
-    .then((item) => res.redirect(item.inputURL))
-    .catch((error) => console.log('error'))
-})
 
 app.listen(port, () => {
   console.log('now is running')
---
+++ routes/modules/home.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const shortenURL = require('../../models/shortenURL')
+const generateRandomString = require('../../generateRandomString')
+const server = `http://localhost:3000/`
+
+//首頁
+router.get('/', (req, res) => {
+  res.render('index', {})
+})
+
+//輸入網址後資料庫先查詢inputURL是否存在，
+//若存在輸出outputURL，若不存在則創建一個存於資料庫中
+router.post('/', (req, res) => {
+  const inputURL = req.body.inputURL
+  let randomString = generateRandomString()
+  const outputURL = server + `${randomString}`
+
+  shortenURL.findOne({ inputURL })
+    .lean()
+    .then((item) => {
+      if (item) {
+        res.render('show', { successURL: item.outputURL })
+      } else {
+        shortenURL.create({ inputURL, outputURL })
+          .then(() => res.render('show', { successURL: outputURL }))
+          .catch(error => { console.log('create error') })
+      }
+    })
+    .catch(error => { console.log('error') })
+})
+
+//短網址輸入後，資料庫查找後直接輸出，連結原始網址
+router.get('/:string', (req, res) => {
+  const string = req.params.string
+  const outputURL = server + string
+  shortenURL.findOne({ outputURL })
+    .then((item) => res.redirect(item.inputURL))
+    .catch((error) => console.log('error'))
+})
+
+
+module.exports = router
